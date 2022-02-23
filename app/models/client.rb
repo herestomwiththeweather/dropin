aa50e7b83a4c6ec0d85b8c50e4987423f72a31a1
Tom Brown <herestomwiththeweather@gmail.com>
2022-02-23 17:18:34 -0600

    testing events api

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,42 @@
+require 'uri'
+require 'cgi'
+require 'net/https'
+
+DASH_BASE_URL = 'https://api.dashplatform.com'
+DASH_VERSION = 'v1'
+
 class Client < ApplicationRecord
+  has_many :access_tokens
+
+  def get_token
+    u = URI.parse(DASH_BASE_URL)
+    http = ::Net::HTTP.new(u.host, u.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    body = {
+      :client_id => CGI::escape(identifier),
+      :client_secret => CGI::escape(secret),
+      :grant_type => 'client_credentials',
+      :user_type => 'company'
+    }.to_json
+    response = http.post("/#{DASH_VERSION}/auth/token?company=#{company}", body, "Content-Type": "application/json\r\n")
+    access_token = JSON.parse(response.body)['access_token']
+    access_tokens.create!(token: access_token)
+    access_token
+  end
+
+  def access_token
+    access_tokens.order('created_at DESC').first
+  end
+
+  def get_events
+    get_token if access_token.nil?
+    u = URI.parse("#{DASH_BASE_URL}/#{DASH_VERSION}/events?company=#{company}")
+    http = ::Net::HTTP.new(u.host, u.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = ::Net::HTTP::Get.new(u.request_uri)
+    request['Authorization'] = "Bearer #{access_token.token}"
+    response = http.request(request)
+  end
 end
