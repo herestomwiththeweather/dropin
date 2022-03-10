d36477aee9ee11a92dbab4e83a652b632e1cc2fc
Tom Brown <herestomwiththeweather@gmail.com>
2022-03-10 15:59:50 -0600

    handle missing upcoming event

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,9 @@
 class PlayersController < ApplicationController
   def index
     @players_event = Event.upcoming_event_by_category(Event::PLAYERS)
-    @players = @players_event.get_people
+    @players = @players_event&.get_people || []
 
     @goalies_event = Event.upcoming_event_by_category(Event::GOALIES)
-    @goalies = @goalies_event.get_people
+    @goalies = @goalies_event&.get_people || []
   end
 end
