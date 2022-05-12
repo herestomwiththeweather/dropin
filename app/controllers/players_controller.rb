c381d13bd671b27519bfd4e2a4453396fde09b9b
Tom Brown <herestomwiththeweather@gmail.com>
2022-05-12 00:34:19 -0500

    use actioncable for today's dropin

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,10 +3,7 @@ class PlayersController < ApplicationController
 
   def index
     @players_event = Event.upcoming_event_by_category(Event::PLAYERS)
-    @players = @players_event&.get_people || []
-
     @goalies_event = Event.upcoming_event_by_category(Event::GOALIES)
-    @goalies = @goalies_event&.get_people || []
   end
 
   def today_count
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -4,4 +4,8 @@ class Registration < ApplicationRecord
 
   validates :event, presence: true
   validates :person, presence: true, uniqueness: { scope: :event_id }
+
+  after_create_commit -> do
+    broadcast_append_later_to(event, :registrations, target: "event_#{event.id}-registrations")
+  end
 end
