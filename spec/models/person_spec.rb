1952d1425de291febdc72e0af6593037f6c55afc
Tom Brown <herestomwiththeweather@gmail.com>
2025-03-06 19:10:24 -0600

    fix person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Person, :type => :model do
   describe "when creating a new person" do
     before do
-      @person = Person.new(name: 'Bobby Orr', identifier: 12345)
+      @client = Client.create(company: 'Boston Garden')
+      @person = Person.new(name: 'Bobby Orr', identifier: 12345, client: @client)
     end
 
     it "should be valid" do
